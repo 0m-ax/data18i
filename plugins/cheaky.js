526582b3c33c154b5d6adc2f230f2d3312f4a3bb
max campbell <maxc@maxc.in>
2018-09-28 19:56:54 +0200

    improved hello and cheaky plugins

--- plugins/cheaky.js
+++ plugins/cheaky.js
@@ -7,6 +7,14 @@ module.exports = function ({settings,registarRegex,client,plugins,registarComman
     registarRegex(/\ud83e\udd84/i,(message)=>{
         message.reply(':heart: :large_orange_diamond:  :yellow_heart: :green_heart: :blue_heart: :purple_heart:')
     })
+    registarRegex(/\ud83d\udc33/i,(message)=>{
+        const attachment = new Attachment('https://developers.redhat.com/blog/wp-content/uploads/2015/01/docker-whale-home-logo.png');
+        message.reply(attachment)
+    })
+    registarRegex(/\ud83d\udc0b/i,(message)=>{
+        const attachment = new Attachment('https://developers.redhat.com/blog/wp-content/uploads/2015/01/docker-whale-home-logo.png');
+        message.reply(attachment)
+    })
     registarRegex(/elon musk/i,(message)=>{
         // Create the attachment using Attachment
         const attachment = new Attachment('https://cdn.discordapp.com/attachments/486433511335657475/493715312911450112/unknown.png');
--- plugins/hello.js
+++ plugins/hello.js
@@ -3,13 +3,30 @@ const data18iNS = trivialdb.ns('data18i');
 const helloDB = data18iNS.db('hello');
 let message = "Hey I am Data18I bot. I live in the cloud floating around on my docker whale guided by kubernetes. If you're weird you can look inside me here https://github.com/0m-ax/data18i (pls dont it hurts)";
 module.exports = async function ({client}){
-    client.on('guildCreate',(guild)=>{
-        guild.defaultChannel.send(message)
+    client.on('guildCreate',async (guild)=>{
+        let dc = await getDefaultChannel(guild);
+        if(dc){
+            dc.send(message)
+        }
     })
     for(let guild of client.guilds){
+        guild = guild[1];
         if(!await helloDB.load(guild.id,false)){
-            await helloDB.save(guild.id,true)
-            guild.defaultChannel.send(message)
+            await helloDB.save(guild.id,true);
+            let dc = await getDefaultChannel(guild);
+            if(dc){
+                dc.send(message)
+            }
         }
     }
 }
+async function getDefaultChannel(guild) {
+    // get "original" default channel
+    if(guild.channels.has(guild.id))
+      return guild.channels.get(guild.id)
+   
+    // Check for a "general" channel, which is often default chat
+    if(guild.channels.exists("name", "general"))
+      return guild.channels.find("name", "general");
+}
+   
