ed65ea305012db48da48ebdbf11d36f326fb6892
max campbell <maxc@maxc.in>
2018-09-28 19:25:04 +0200

    made hello work for existing guilds

--- plugins/hello.js
+++ plugins/hello.js
@@ -1,8 +1,15 @@
 const trivialdb = require('trivialdb');
 const data18iNS = trivialdb.ns('data18i');
 const helloDB = data18iNS.db('hello');
-module.exports = function ({client}){
+let message = "Hey I am Data18I bot. I live in the cloud floating around on my docker whale guided by kubernetes. If you're weird you can look inside me here https://github.com/0m-ax/data18i (pls dont it hurts)";
+module.exports = async function ({client}){
     client.on('guildCreate',(guild)=>{
-        guild.defaultChannel.send("Hey I am Data18I bot. I live in the cloud floating around on my docker whale guided by kubernetes. If you're weird you can look inside me here https://github.com/0m-ax/data18i (pls dont it hurts)")
+        guild.defaultChannel.send(message)
     })
+    for(let guild of client.guilds){
+        if(!await helloDB.load(guild.id,false)){
+            await helloDB.save(guild.id,true)
+            guild.defaultChannel.send(message)
+        }
+    }
 }
