e540db9e8ce2e953dbee0de5ddf9bc6f822925b2
max campbell <maxc@maxc.in>
2018-10-31 09:10:59 +0100

    improved slammer

--- plugins/cheaky.js
+++ plugins/cheaky.js
@@ -3,6 +3,32 @@ const fetch = require('node-fetch')
 const {VM} = require('vm2');
 const lolcode = require('lolcode/parser')
 module.exports = function ({settings,registarRegex,client,plugins,registarCommand}){
+    registarCommand('jail',(message)=>{
+        if(message.author.id == "489389057311506433"){
+            for(let user of message.mentions.users.array()){
+                jail(user,message);
+            }
+        }else{
+            message.reply("Oy you ant my master off to jail with you")
+            jail(message.author,message)
+            setTimeout(()=>{
+                unjail(message.author,message)
+            },1000*60*5)
+        }
+    })
+    registarCommand('unjail',(message)=>{
+        if(message.author.id == "489389057311506433"){
+            for(let user of message.mentions.users.array()){
+                unjail(user,message);
+            }
+        }else{
+            message.reply("Oy you ant my master off to jail with you")
+            unjail(message.author,message)
+            setTimeout(()=>{
+                jail(message.author,message)
+            },1000*60*5)
+        }
+    })
     registarRegex(/\ud83c\udf46/i,(message)=>{
         message.reply(':wink:')
     })
@@ -23,33 +49,32 @@ module.exports = function ({settings,registarRegex,client,plugins,registarComman
         // Send the attachment in the message channel
         message.channel.send(attachment);
     })
+    function jail(user,message){
+        const attachment = new Attachment('https://media.discordapp.net/attachments/486433511335657475/498747267755540480/unknown.png');
+
+        let role = message.guild.roles.find(r => r.name === "Jailed");
+        if(role){
+            message.member.addRole(role)
+        }
+        let channel = client.channels.find(r => r.name === "the-shower")
+        if(channel){
+            channel.send('Oy '+message.author.toString()+' ye bloodie twit in te slammer with ya.',{
+                files:[attachment]
+            })
+        }
+    }
+    function unjail(user,message){
+        let role = message.guild.roles.find(r => r.name === "Jailed");
+        message.member.removeRole(role)
+    }
     // registarRegex(/\+cow/i,async (message)=>{
     //     const attachment = new Attachment('https://source.unsplash.com/random/?cow');        
     //     message.channel.send(attachment);
     // })
     registarRegex(/w\s*a\s*t\s*e\s*r\s*f\s*a(\s*l|i){2}/i,function(message){
-        const attachment = new Attachment('https://media.discordapp.net/attachments/486433511335657475/498747267755540480/unknown.png');
-        let user = message.member.user;
-        let role = message.guild.roles.find(r => r.name === "Jailed");
-        if(role){
-            message.member.addRole(role)
-            setTimeout(()=>{
-                message.member.removeRole(role)
-            },1000*60)
-        }else{
-            console.log("Nope role not found");
-        }
+        jail(message.author,message);
         setTimeout(()=>{
-            let channel = client.channels.find(r => r.name === "the-shower")
-            if(channel){
-                channel.send('Oy '+message.author.toString()+' ye bloodie idiot in te slammer with ya.',{
-                    files:[attachment]
-                })
-            }else{
-                message.channel.send('Oy '+message.author.toString()+' ye bloodie idiot in te slammer with ya.',{
-                    files:[attachment]
-                })
-            }
+            unjail(message.author,message)
         });
 
     },{latinise:true});
